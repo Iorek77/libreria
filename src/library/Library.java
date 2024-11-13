d7562807199068726a4e687b21a90f241fd28959
Iorek <Iorek@DESKTOP-RBH5HHM>
2024-11-13 06:41:14 -0300

    Library 1

--- src/library/Library.java
+++ src/library/Library.java
@@ -4,13 +4,26 @@ import java.util.Scanner;
 
 public class Library {
 
+    static Scanner input = new Scanner(System.in);
     private static book LOTR = new book("Lord of the Rings", "Tolkien", "Fantasy", 1);
     private static book DQ = new book("Don Quixote", "Miguel de Cervantes", "Novel", 2);
     private static book Shogun = new book("Shogun", "James Clavell", "Historical Fiction", 3);
+    
+    public static void addbook(){
+    String name, author, genre;
+        System.out.println("Nombre del libro: ");
+        name = input.next();
+        System.out.println("Genero: ");
+        genre = input.next();
+        System.out.println("Autor del libro: ");
+        author = input.next();
+        
+        
+    }
 
     public static void registro() {
-        Scanner input = new Scanner(System.in);
-        String username, nickname, password;
+        
+        String username, nickname, password, email, phone, id;
         System.out.println("Registro de cuenta.");
         System.out.println("Ingrese un nombre de usuario.");
         username = input.next();
@@ -18,7 +31,13 @@ public class Library {
         password = input.next();
         System.out.println("Ingrese un nombre de perfil.");
         nickname = input.next();
-        user user01 = new user(nickname, username, password);
+        System.out.println("Ingrese un email.");
+        email = input.next();
+        System.out.println("Ingrese un numero de telefono.");
+        phone = input.next();
+        System.out.println("Ingrese cedula.");
+        id = input.next();
+        user user01 = new user(nickname, username, password, email, phone, id);
         System.out.println("Datos de su cuenta: ");
         System.out.println("Nombre de usuario");
         System.out.println(user01.getUsername());
@@ -26,6 +45,13 @@ public class Library {
         System.out.println(user01.getPassword());
         System.out.println("Nombre de perfil");
         System.out.println(user01.getNickname());
+        System.out.println("E-Mail");
+        System.out.println(user01.getEmail());
+        System.out.println("Telefono");
+        System.out.println(user01.getPhone());
+        System.out.println("Cedula");
+        System.out.println(user01.getId());
+        
     }
 
     public static void catalogo() {
@@ -37,7 +63,6 @@ public class Library {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         registro();
         catalogo();
         int selection = input.nextInt();
--- src/library/user.java
+++ src/library/user.java
@@ -3,12 +3,39 @@ package library;
 
 
 public class user {
-    String nickname, username, password;
+    String nickname, username, password, email, phone, id;
 
-    public user(String nickname, String username, String password) {
+    public user(String nickname, String username, String password, String email, String phone, String id) {
         this.nickname = nickname;
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getNickname() {
